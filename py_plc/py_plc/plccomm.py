de8e5e6d20b7069330a1177d1c6f04f023fd855f
jeyong <jeyong@gmail.com>
2024-10-19 22:24:54 +0900

    add packet maker

--- py_plc/py_plc/plccomm.py
+++ py_plc/py_plc/plccomm.py
@@ -10,9 +10,11 @@ class ActuatorSubscriber(Node):
     def __init__(self):
         super().__init__('actuator_subscriber')
         self.px4_listen_port = 2006
-        self.plc_ip = '192.168.2.77'
+        self.plc_ip = '192.168.2.88'
         self.plc_port = 2005
+        self.px4_ip = '127.0.0.1' # self.px4_ip = '192.168.2.200'
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind((self.px4_ip, self.px4_listen_port))
         self.plcPacket = PLCPacket()
         self.subscription = self.create_subscription(
             ActuatorOutputs,
@@ -23,10 +25,12 @@ class ActuatorSubscriber(Node):
 
     def listener_callback(self, msg):
         # 수신하면 메시지를 받아서 plc로 전송
-        # socket 생성해서 
-        pass 
-
-
+        # socket 생성해서
+        throttle = msg.actuator[3]
+        steering = msg.actuator[1]
+        self.sock.sendto(self.plcPacket.makeWritePacket2(throttle, steering), (self.plc_ip, self.plc_port))
+        
+        
 def main(args=None):
     rclpy.init(args=args)
 
