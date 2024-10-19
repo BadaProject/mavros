087f8af382094f2e251ef3c7e41a08e71517e2bb
jeyong <jeyong@gmail.com>
2024-10-19 17:41:42 +0900

    add actuator_outputs

--- mavros/src/plugins/actuator_outputs.cpp
+++ mavros/src/plugins/actuator_outputs.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Marcel Stüttgen <stuettgen@fh-aachen.de>
+ * Copyright 2015 Andreas Antener <andreas@uaventure.com>.
  * Copyright 2021 Vladimir Ermakov.
  *
  * This file is part of the mavros package and subject to the license terms
@@ -7,13 +7,15 @@
  * https://github.com/mavlink/mavros/tree/master/LICENSE.md
  */
 /**
- * @brief ActuatorOutputs plugin
+ * @brief ActuatorOutputsPlugin plugin
  * @file actuator_outputs.cpp
  * @author Jeyong Shin <jeyong@subak.io>
- * 
+ *
  * @addtogroup plugin
  * @{
  */
+
+#include <string>
 
 #include "rcpputils/asserts.hpp"
 #include "mavros/mavros_uas.hpp"
@@ -26,13 +28,10 @@ namespace mavros
 {
 namespace std_plugins
 {
-using namespace std::placeholders;      // NOLINT
 
 /**
- * @brief ActuatorControl plugin
- * @plugin actuator_control
- *
- * Sends actuator controls to FCU controller.
+ * @brief Altitude plugin.
+ * @plugin altitude
  */
 class ActuatorOutputsPlugin : public plugin::Plugin
 {
@@ -40,53 +39,33 @@ public:
   explicit ActuatorOutputsPlugin(plugin::UASPtr uas_)
   : Plugin(uas_, "actuator_outputs")
   {
+
     auto sensor_qos = rclcpp::SensorDataQoS();
 
-    actuator_outputs_pub = node->create_publisher<mavros_msgs::msg::ActuatorOutputs>(
-      "target_actuator_outputs", sensor_qos);
-    actuator_outputs_sub = node->create_subscription<mavros_msgs::msg::ActuatorOutputs>(
-      "actuator_outputs", sensor_qos, std::bind(
-        &ActuatorOutputsPlugin::actuator_outputs_cb, this, _1));
+    actuator_outputs_pub = node->create_publisher<mavros_msgs::msg::ActuatorOutputs>("actuator_outputs", sensor_qos);
   }
 
   Subscriptions get_subscriptions() override
   {
     return {
-      make_handler(&ActuatorOutputsPlugin::handle_actuator_control_target),
+      make_handler(&ActuatorOutputsPlugin::handle_actuator_outputs),
     };
   }
 
 private:
-  rclcpp::Publisher<mavros_msgs::msg::ActuatorControl>::SharedPtr actuator_outputs_pub;
-  rclcpp::Subscription<mavros_msgs::msg::ActuatorControl>::SharedPtr actuator_outputs_sub;
+  std::string frame_id;
 
-  /* -*- rx handlers -*- */
+  rclcpp::Publisher<mavros_msgs::msg::ActuatorOutputs>::SharedPtr actuator_outputs_pub;
 
-  void handle_actuator_control_target(
+  void handle_actuator_outputs(
     const mavlink::mavlink_message_t * msg [[maybe_unused]],
-    mavlink::common::msg::ACTUATOR_CONTROL_TARGET & act,
-    plugin::filter::ComponentAndOk filter [[maybe_unused]])
+    mavlink::common::msg::ActuatorOutputs & actuator_outputs, plugin::filter::SystemAndOk filter [[maybe_unused]])
   {
-    auto ract = mavros_msgs::msg::ActuatorControl();
-    ract.header.stamp = uas->synchronise_stamp(act.time_usec);
-    ract.group_mix = act.group_mlx;
-    ract.controls = act.controls;
+    auto ros_msg = mavros_msgs::msg::ActuatorOutputs();
+    ros_msg.outputs.active = actuator_outputs.active;
+    ros_msg.outputs = actuator_outputs.outputs;
 
-    actuator_outputs_pub->publish(ract);
-  }
-
-  /* -*- callbacks -*- */
-
-  void actuator_outputs_cb(const mavros_msgs::msg::ActuatorOutputs::SharedPtr req)
-  {
-    //! about groups, mixing and channels: @p https://pixhawk.org/dev/mixing
-    //! message definiton here: @p https://mavlink.io/en/messages/common.html#ACTUATOR_OUTPUT_STATUS
-    mavlink::common::msg::ACTUATOR_OUTPUT_STATUS aout{};
-    act.time_usec = get_time_usec(req->header.stamp);
-    uas->msg_set_target(act);
-    act.controls = req->outputs;
-
-    uas->send_message(act);
+    actuator_outputs_pub->publish(ros_msg);
   }
 };
 
