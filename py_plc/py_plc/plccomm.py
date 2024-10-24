5adcf454b0ce864618744a123cd743c3e09f4620
jeyong <jeyong@gmail.com>
2024-10-24 17:48:53 +0900

    change mavros actuator outputs

--- py_plc/py_plc/plccomm.py
+++ py_plc/py_plc/plccomm.py
@@ -1,3 +1,5 @@
+import threading
+from time import sleep
 import rclpy
 from rclpy.node import Node
 import socket
@@ -21,8 +23,10 @@ class ActuatorSubscriber(Node):
             'mavros/actuator_outputs',
             self.listener_callback,
             10)
-        self.subscription  # prevent unused variable warning
-
+        # thread로 0.5초마다 send_read_request 메소드를 실행
+        self.create_timer(0.5, self.send_read_request)
+        # thread로 handle_read_response 메소드를 실행
+        thread = threading.Thread(target=self.handle_read_response, args=(self.sock,))
     def listener_callback(self, msg):
         # 수신하면 메시지를 받아서 plc로 전송
         # socket 생성해서
@@ -32,6 +36,36 @@ class ActuatorSubscriber(Node):
         steering = int(msg.actuator[1])
         sock.sendto(self.plcPacket.makeWritePacket(), (self.plc_ip, self.plc_port))
         print("sending: ", throttle, steering)
+
+    # 0.5초마다 plc에게 'hello' 메시지를 보내는 send_read_request 메소드를 생성
+    def send_read_request(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(self.plcPacket.makeReadPacket(), (self.plc_ip, self.plc_port))
+        print("sending read request")
+        
+    def handle_read_response(self, sock):
+        # plc로부터 응답을 받아서 처리하는 메소드
+        # plc로부터 받은 데이터를 출력
+
+        px4_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        px4_socket.bind(('', self.px4_listen_port))
+        while True:
+            data, addr = px4_socket.recvfrom(1024)
+            if data[20] == self.plc_packet.RESPONSE_READ:
+                if data[19] == self.plc_packet.getCheckSum(data, 0, 19):
+                    print('-----PLC RESPONSE_READ Received!!! -------------')
+                    plc_packet = PlcToPx4Packet()
+                    plc_packet.parseDataBytes(data[32:])
+                    plc_packet.printData()
+                    # 32번째 index부터 30bytes 데이터를 short int 15개에 담는다.
+                    # publish 
+            print(data)
+            print(f"Received data: {data} from {addr} length : {len(data)}")
+
+        # data에서 read-request에 대한 응답인지 확인
+        # data를 parsing해서 publish
+        
+        print(data)
 
 def main(args=None):
     rclpy.init(args=args)
